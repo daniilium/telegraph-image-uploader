649c57454386c1f88bd362ce0dde8c2ca19b622c
u1f5a4 <73600007+u1f5a4@users.noreply.github.com>
2022-08-25 08:02:37 +0300

    fix: remove files

--- packages/main/src/getAppFolder.ts
+++
@@ -1,6 +0,0 @@
-import * as os from "os";
-
-export const getAppFolder = () => {
-  const platform = os.platform();
-  if (platform === "darwin") return __dirname;
-};
--- packages/renderer/src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-export {};
-
-declare global {
-  interface Window {
-    electronAPI: any;
-  }
-}
