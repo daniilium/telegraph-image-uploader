52b71589766c7f40e2daf6b6515bc8f6c3aee2b8
u1f5a4 <73600007+u1f5a4@users.noreply.github.com>
2022-08-26 18:57:44 +0300

    fix: change BrowserRouter to HashRouter

--- packages/renderer/src/main.tsx
+++ packages/renderer/src/main.tsx
@@ -1,22 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
+import "../index.css";
 import { StepOne } from "./StepOne";
-import { StepThree } from "./StepThree";
 import { StepTwo } from "./StepTwo";
+import { StepThree } from "./StepThree";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route index element={<StepOne />} />
         <Route path="step-two" element={<StepTwo />} />
         <Route path="step-three" element={<StepThree />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
