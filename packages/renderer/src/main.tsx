8fbad325a8ad0e1ddf6a0cef7bc5035dbd3790b8
u1f5a4 <73600007+u1f5a4@users.noreply.github.com>
2022-09-02 20:25:37 +0300

    feat: add 🐳 mantine.dev

--- packages/renderer/src/main.tsx
+++ packages/renderer/src/main.tsx
@@ -1,23 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter, Route, Routes } from "react-router-dom";
+import { MantineProvider } from "@mantine/core";
 
-import "../index.css";
-import { StepOne } from "./StepOne";
-import { StepTwo } from "./StepTwo";
-import { StepThree } from "./StepThree";
+// import "../index.css";
+import { StepOne } from "./pages/Step1";
+import { StepThree } from "./pages/Step3";
+import { StepTwo } from "./pages/Step2";
+import { theme } from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route index element={<StepOne />} />
-        <Route path="step-two" element={<StepTwo />} />
-        <Route path="step-three" element={<StepThree />} />
-      </Routes>
-    </HashRouter>
+    <MantineProvider withGlobalStyles withNormalizeCSS theme={theme}>
+      <HashRouter>
+        <Routes>
+          <Route index element={<StepOne />} />
+          <Route path="step-two" element={<StepTwo />} />
+          <Route path="step-three" element={<StepThree />} />
+        </Routes>
+      </HashRouter>
+    </MantineProvider>
   </React.StrictMode>
 );
---
+++ packages/renderer/src/theme.ts
@@ -0,0 +1,23 @@
+export const colors = {
+  black: "#333333",
+  gray: "#79828b",
+  red: "#ca3737",
+};
+
+export const fonts = {
+  title: "'Roboto', sans-serif",
+  main: "'EB Garamond', serif",
+};
+
+export const theme = {
+  fontFamily: fonts.main,
+  fontSizes: {
+    md: 18,
+  },
+  headings: {
+    fontFamily: fonts.title,
+    sizes: {
+      h1: { fontSize: 32 },
+    },
+  },
+};
